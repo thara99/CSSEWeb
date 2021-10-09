ca169bfa36b6f402074c9118cd26bed786fdd344
thara99 <it19148014@my.sliit.lk>
2021-10-09 13:11:37 +0530

    completeFrontEndFinal

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -51,10 +51,9 @@ export default function SignIn() {
               margin="normal"
               required
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
+              id="pNum"
+              label="Phone Number"
+              name="pNum"
               autoFocus
             />
             <TextField
